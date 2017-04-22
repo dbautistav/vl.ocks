ff3e888200ec5f71aa4c6fc54d8bfcb027adb6e5
David Bautista <dbautistav@gmail.com>
2017-04-22 00:29:37 -0500

    Add 'visualization' component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { VisualizationComponent } from './visualization/visualization.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    VisualizationComponent
   ],
   imports: [
     BrowserModule,
